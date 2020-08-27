44bc333180a07592c07eecb25b2e2f280fb17ccb
delola\delola lokal <aleksandra.man@gmail.com>
2020-08-27 14:32:49 +0200

    Codingbat task: hasTeen JUnit test + method

--- src/main/java/pl/levelupyourjava/codingbat/WarmUp1.java
+++ src/main/java/pl/levelupyourjava/codingbat/WarmUp1.java
@@ -98,7 +98,10 @@ public class WarmUp1 {
      * hasTeen(20, 10, 13) → true
      */
     public boolean hasTeen(int a, int b, int c) {
-        return false;
+
+        return (a >= 13 && a <= 19 ||
+            b >= 13 && b <= 19 ||
+            c >= 13 && c <= 19);
     }
 
     /**
