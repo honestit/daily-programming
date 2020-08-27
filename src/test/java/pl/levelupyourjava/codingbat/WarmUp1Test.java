76b03d10946c99403d7e554dd34b6d4996386040
delola\delola lokal <aleksandra.man@gmail.com>
2020-08-27 12:25:03 +0200

    generation of empty JUnit tests

---
+++ src/test/java/pl/levelupyourjava/codingbat/WarmUp1Test.java
@@ -0,0 +1,149 @@
+package pl.levelupyourjava.codingbat;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+public class WarmUp1Test {
+
+    private WarmUp1 warmUp1;
+
+    @Before
+    public void setUp() throws Exception {
+        warmUp1 = new WarmUp1();
+    }
+
+    @Test
+    public void sleepIn() {
+        boolean result = warmUp1.sleepIn(false, false);
+        assertThat(result, is(true));
+        result = warmUp1.sleepIn(true, false);
+        assertThat(result, is(false));
+        result = warmUp1.sleepIn(false, true);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void diff21() {
+    }
+
+    @Test
+    public void nearHundred() {
+    }
+
+    @Test
+    public void missingChar() {
+    }
+
+    @Test
+    public void backAround() {
+    }
+
+    @Test
+    public void startHi() {
+    }
+
+    @Test
+    public void hasTeen() {
+    }
+
+    @Test
+    public void mixStart() {
+    }
+
+    @Test
+    public void close10() {
+    }
+
+    @Test
+    public void stringE() {
+    }
+
+    @Test
+    public void everyNth() {
+    }
+
+    @Test
+    public void monkeyTrouble() {
+    }
+
+    @Test
+    public void parrotTrouble() {
+    }
+
+    @Test
+    public void posNeg() {
+    }
+
+    @Test
+    public void frontBack() {
+    }
+
+    @Test
+    public void or35() {
+    }
+
+    @Test
+    public void icyHot() {
+    }
+
+    @Test
+    public void loneTeen() {
+    }
+
+    @Test
+    public void startOz() {
+    }
+
+    @Test
+    public void in3050() {
+    }
+
+    @Test
+    public void lastDigit() {
+    }
+
+    @Test
+    public void sumDouble() {
+    }
+
+    @Test
+    public void makes10() {
+    }
+
+    @Test
+    public void notString() {
+    }
+
+    @Test
+    public void front3() {
+    }
+
+    @Test
+    public void front22() {
+    }
+
+    @Test
+    public void in1020() {
+    }
+
+    @Test
+    public void delDel() {
+    }
+
+    @Test
+    public void intMax() {
+    }
+
+    @Test
+    public void max1020() {
+    }
+
+    @Test
+    public void endUp() {
+    }
+}
